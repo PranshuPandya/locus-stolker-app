83e9adb13f9559359ba3e79c9b07a1104424f490
TESLA2402 <83589511+TESLA2402@users.noreply.github.com>
2021-12-06 15:35:27 +0530

    Update group_member.dart

--- lib/screens/group_member.dart
+++ lib/screens/group_member.dart
@@ -318,15 +318,30 @@ class GroupRectangle extends StatelessWidget {
                 ),
               ),
             ),
-            Padding(
+             Padding(
               padding: EdgeInsets.symmetric(
                 vertical: 10.0,
                 horizontal: 8.0,
               ),
-              child: Text(
-                '$memberName',
-                style: TextStyle(
-                  fontSize: 20.0,
+              child: Container(
+                padding: EdgeInsets.all(4.0),
+                height: 50.0,
+                width: 250,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(
+                    color: Colors.black,
+                    width: 6,
+                  ),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Text(
+                  '$memberName',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20.0,
+                  ),
                 ),
               ),
             ),
